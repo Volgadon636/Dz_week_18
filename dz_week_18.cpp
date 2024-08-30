80e1ced884451f11b773f2986f4303760146dba6
Volgadon636 <dtochilin108@gmail.com>
2024-08-30 18:08:02 +0300

    ready 1 task

--- dz_week_18.cpp
+++ dz_week_18.cpp
@@ -1,5 +1,4 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 class complex_num {
     int i = sqrt(-1);
@@ -40,38 +39,89 @@ public:
 
     }
 };
+
 void complex_sum(complex_num z1, complex_num z2) {
+
     int sum_1 = z1.a + z2.a;
     int sum_2 = z1.b + z2.b;
-    cout << "sum1 = " << sum_1 << ' ' << "sum2 = " << sum_2 << endl;
     if (sum_1 == 0 && sum_2 == 0) {
         cout << "the sum is: " << 0 << endl;
     }
     else if (sum_2 > 0) {
-        cout << "the sum is: " << sum_1<<" + " << sum_2 << "i" << endl;
+        cout << "the sum is: " << sum_1 << " + " << sum_2 << "i" << endl;
     }
     else if (sum_2 < 0) {
-        cout << "the sum is: " << sum_1<<" - " << sum_2*(-1) << "i" << endl;
+        cout << "the sum is: " << sum_1 << " - " << sum_2 * (-1) << "i" << endl;
     }
-    else if (sum_2 == 0){
-        cout << "the sum is: " << sum_1<< endl;
+    else if (sum_2 == 0) {
+        cout << "the sum is: " << sum_1 << endl;
     }
-    
-    
-    
-    
 }
+
+
+void complex_min(complex_num z1, complex_num z2) {
+
+    int min_1 = z1.a - z2.a;
+    int min_2 = z1.b - z2.b;
+
+    if (min_1 == 0 && min_2 == 0) {
+        cout << "the min is: " << 0 << endl;
+    }
+    else if (min_1 == 0) {
+        cout << "the min is: " << min_2 << "i" << endl;
+    }
+    else if (min_2 > 0) {
+        cout << "the min is: " << min_1 << " + " << min_2 << "i" << endl;
+    }
+    else if (min_2 < 0) {
+        cout << "the min is: " << min_1 << " - " << min_2 * (-1) << "i" << endl;
+    }
+    else if (min_2 == 0) {
+        cout << "the min is: " << min_1 << endl;
+    }
+}
+
+void complex_mul(complex_num z1, complex_num z2) {
+    int part1 = z1.a * z2.a - z1.b * z2.b;
+    int part2 = z1.b * z2.a + z1.a * z2.b;
+    if (part1 == 0 && part2 == 0) {
+        cout << "the mul is: " << 0 << endl;
+    }
+    else if (part1 == 0) {
+        cout << "the mul is: " << part2 << "i" << endl;
+    }
+    else if (part2 == 0) {
+        cout << "the mul is: " << part1 << endl;
+    }
+    else {
+        cout << "the mul is: " << part1 << " + " << part2 << "i" << endl;
+    }
+
+}
+void complex_div(complex_num z1, complex_num z2) {
+    //Формула здесь сложная, поэтому осуществлю только вывод формулы
+    cout << "the div is: " << z1.a << " + " << z1.b << "i / " << z2.a << " + " << z2.b << "i = ( " << z1.a << " + " << z1.b << "i ) (" << z2.a << "-" << z2.b << "i ) / " << z2.a << "^2 + " << z2.b << "^2" << "* i" << endl;
+
+}
+
 int main()
 {
-    int g;
-    int a, b,c,d;
-    cout << "enter first complex num (a and b): "; cin >> a >> b;
-    cout << "enter second complex num (c and d): "; cin >> c>> d;
-    complex_num z1(a, b);
-    complex_num z2(c, d);
-    cout << "\tFIRST\n\n";
-    z1.cout_com_num(z1.a, z1.b);
-    cout << "\tSECOND\n\n";
-    z2.cout_com_num(z2.a, z2.b);
-    complex_sum(z1, z2);
+
+    {
+        cout << "\texercise 1\n\n";
+        int a, b, c, d;
+        cout << "enter first complex num (a and b): "; cin >> a >> b;
+        cout << "enter second complex num (c and d): "; cin >> c >> d;
+        complex_num z1(a, b);
+        complex_num z2(c, d);
+        cout << "\tFIRST\n\n";
+        z1.cout_com_num(z1.a, z1.b);
+        cout << "\n\tSECOND\n\n";
+        z2.cout_com_num(z2.a, z2.b);
+        cout << "\n\n";
+        complex_sum(z1, z2);
+        complex_min(z1, z2);
+        complex_mul(z1, z2);
+        complex_div(z1, z2);
+    }
 }
